75d7272019eb76a5a380478d757dd639f1adaa62
Muhammad Shamil Shaikh <sdeveloper471@gmail.com>
2026-01-28 10:13:20 -0800

    Add files via upload

---
+++ PProject - Copy/app.js
@@ -0,0 +1,14 @@
+function updateTime() {
+    let now = new Date();
+
+    let hours = String(now.getHours()).padStart(2, "0");
+    let minutes = String(now.getMinutes()).padStart(2, "0");
+    let seconds = String(now.getSeconds()).padStart(2, "0");
+
+    document.getElementById("time").innerText =
+        `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
