a1ddccebe706c832d7914b8ab094938ad35f7189
Muhammad Shamil Shaikh <sdeveloper471@gmail.com>
2026-02-10 10:01:36 -0800

    Add files via upload

---
+++ password-generator/app.js
@@ -0,0 +1,11 @@
+function generatePassword() {
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let password = "";
+
+    for (let i = 0; i < 10; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    document.getElementById("password").value = password;
+}
