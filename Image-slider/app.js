e57816d875e9883b0e2d6e276574c836c660e326
Muhammad Shamil Shaikh <sdeveloper471@gmail.com>
2026-02-16 11:00:02 -0800

    Add files via upload

--- Image-slider/app.js
+++ Image-slider/app.js
@@ -1,15 +1,19 @@
-let index = 1;
-const img = document.getElementById("slide");
+const images = [
+  "https://picsum.photos/600/350?1",
+  "https://picsum.photos/600/350?2",
+  "https://picsum.photos/600/350?3",
+  "https://picsum.photos/600/350?4"
+];
+
+let index = 0;
+const slide = document.getElementById("slide");
 
 function next() {
-    index++;
-    img.src = `https://picsum.photos/600/350?random=${index}`;
+  index = (index + 1) % images.length;
+  slide.src = images[index];
 }
 
 function prev() {
-    index--;
-    if (index < 1) index = 1;
-    img.src = `https://picsum.photos/600/350?random=${index}`;
+  index = (index - 1 + images.length) % images.length;
+  slide.src = images[index];
 }
-
-
