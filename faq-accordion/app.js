cb85db8451cc363f337edf73f6fb455c612f5359
Muhammad Shamil Shaikh <sdeveloper471@gmail.com>
2026-02-19 12:51:45 -0800

    Add files via upload

---
+++ faq-accordion/app.js
@@ -0,0 +1,9 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(q => {
+  q.addEventListener("click", () => {
+    const answer = q.nextElementSibling;
+    answer.style.display =
+      answer.style.display === "block" ? "none" : "block";
+  });
+});
