5380d390547e28aee9ab9b5e49c29d9bbcf9a0aa
Muhammad Shamil Shaikh <sdeveloper471@gmail.com>
2026-03-03 14:57:53 -0800

    Add files via upload

---
+++ Modern Password Generator Website/app.js
@@ -0,0 +1,23 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let password = "";
+
+    if (length < 4) {
+        alert("Password length must be at least 4");
+        return;
+    }
+
+    for (let i = 0; i < length; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password copied!");
+}
