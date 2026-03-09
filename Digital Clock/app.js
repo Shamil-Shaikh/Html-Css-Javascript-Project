89eb52026ff7e1f1532f3f341597369d8f2ebca2
Muhammad Shamil Shaikh <sdeveloper471@gmail.com>
2026-03-08 18:27:21 -0700

    Add files via upload

---
+++ Digital Clock/app.js
@@ -0,0 +1,25 @@
+function updateClock(){
+
+let now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+hours = hours < 10 ? "0"+hours : hours;
+minutes = minutes < 10 ? "0"+minutes : minutes;
+seconds = seconds < 10 ? "0"+seconds : seconds;
+
+let time = hours + ":" + minutes + ":" + seconds;
+
+document.getElementById("clock").textContent = time;
+
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+document.getElementById("date").textContent = now.toLocaleDateString(undefined, options);
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
